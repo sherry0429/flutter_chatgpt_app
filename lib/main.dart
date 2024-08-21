4a64bbf6f5e6a04ea13887f3e31ce9368cd934bd
PickMeUp <sherry0429@users.noreply.github.com>
2024-08-21 18:07:32 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,61 +9,10 @@ import 'conversation_provider.dart';
 import 'popmenu.dart';
 
 
-class NewSystemProxy {
-
-  static const MethodChannel _channel = MethodChannel('system_proxy');
-
-  static Future<Map<String, String>?> getProxySettings() async {
-    var proxy = {
-      "host": "",
-      "port": ""
-    };
-    if (Platform.isAndroid) {
-      String proxyStr = await _channel.invokeMethod('getProxySettings');
-      print("hi $proxyStr");
-      if (proxyStr != "") {
-        List<String> args = proxyStr.split(":");
-        proxy['host'] = args[0];
-        proxy['port'] = args[1];
-      }
-    }
-    else if (Platform.isWindows) {
-      String proxyStr = await _channel.invokeMethod('getProxySettings');
-      if (proxyStr != ""){
-        List<String> args = proxyStr.split(":");
-        proxy['host'] = args[0];
-        proxy['port'] = args[1];
-      }
-    }
-    return proxy;
-  }
-}
-
-
-class ProxiedHttpOverrides extends HttpOverrides {
-  String _port;
-  String _host;
-  ProxiedHttpOverrides(this._host, this._port);
-
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-    // set proxy
-      ..findProxy = (uri) {
-        if (_host != "" && _port != "") {
-          return _host != null ? "PROXY $_host:$_port;" : 'DIRECT';
-        } else {
-          return "DIRECT";
-        }
-      };
-  }
-}
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  dynamic proxy = await NewSystemProxy.getProxySettings();
-  HttpOverrides.global = ProxiedHttpOverrides(proxy['host']!, proxy['port']!);
 
   runApp(
     ChangeNotifierProvider(
