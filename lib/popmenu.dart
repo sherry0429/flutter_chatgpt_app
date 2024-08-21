d8500d2f6b2e9ffb0a0909cdb5b096ad826c5433
PickMeUp <sherry0429@users.noreply.github.com>
2024-08-21 18:08:13 +0800

    Update popmenu.dart

--- lib/popmenu.dart
+++ lib/popmenu.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'conversation_provider.dart';
 import 'models.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+
 
 class CustomPopupMenu extends StatelessWidget {
   @override
@@ -30,71 +34,157 @@ class CustomPopupMenu extends StatelessWidget {
             title: Text('Delete'),
           ),
         ),
+        const PopupMenuItem(
+          value: "set_background",
+          child: ListTile(
+            leading: Icon(Icons.image),
+            title: Text('Set Background'),
+          ),
+        ),
+        const PopupMenuItem(
+          value: "edit_systeminfo",
+          child: ListTile(
+            leading: Icon(Icons.edit_note),
+            title: Text('Edit System Info'),
+          ),
+        ),
       ],
+      
       elevation: 2,
       padding: EdgeInsets.zero,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      onSelected: (value) {
+      onSelected: (value) async {
         if (value == "rename") {
-          // rename
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              String newName = '';
-              return AlertDialog(
-                title: const Text('Rename Conversation'),
-                content: TextField(
-                  // display the current name of the conversation
-                  decoration: InputDecoration(
-                    hintText: Provider.of<ConversationProvider>(context,
-                            listen: false)
-                        .currentConversation
-                        .title,
-                  ),
-                  onChanged: (value) {
-                    newName = value;
-                  },
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: const Text('Cancel'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  TextButton(
-                    child: const Text(
-                      'Rename'
-                      , style: TextStyle(
-                        color: Color(0xff55bb8e),
-                      ),
-                      ),
-                    onPressed: () {
-                      // Call renameConversation method here with the new name
-                      Provider.of<ConversationProvider>(context, listen: false)
-                          .renameConversation(newName);
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              );
-            },
-          );
-
-          // Provider.of<ConversationProvider>(context, listen: false)
-          //     .renameCurrentConversation();
+          await _renameConversation(context);
         } else if (value == "delete") {
           // delete
-          Provider.of<ConversationProvider>(context, listen: false)
+          await Provider.of<ConversationProvider>(context, listen: false)
               .removeCurrentConversation();
         } else if (value == "refresh") {
           // refresh
-          Provider.of<ConversationProvider>(context, listen: false)
+          await Provider.of<ConversationProvider>(context, listen: false)
               .clearCurrentConversation();
+        } else if (value == "set_background") {
+          _setBackground(context);
+        } else if (value == "edit_systeminfo") {
+          _editSystemInfo(context);
         }
       },
     );
   }
+
+
+  Future<void> _renameConversation(BuildContext context) async {
+    String newName = '';
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Rename Conversation'),
+          content: TextField(
+            decoration: InputDecoration(
+              hintText: Provider.of<ConversationProvider>(context, listen: false)
+                  .currentConversation
+                  .title,
+            ),
+            onChanged: (value) {
+              newName = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: const Text(
+                'Rename',
+                style: TextStyle(
+                  color: Color(0xff55bb8e),
+                ),
+              ),
+              onPressed: () async {
+                await Provider.of<ConversationProvider>(context, listen: false)
+                    .renameConversation(newName);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  Future<void> _setBackground(BuildContext context) async {
+    if (Platform.isAndroid || Platform.isIOS) {
+      final ImagePicker _picker = ImagePicker();
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        Provider.of<ConversationProvider>(context, listen: false)
+            .setBackgroundImage(image.path);
+      }
+    } else {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.image,
+        allowMultiple: false,
+      );
+      if (result != null) {
+        Provider.of<ConversationProvider>(context, listen: false)
+            .setBackgroundImage(result.files.single.path!);
+      }
+    }
+  }
+
+  void _editSystemInfo(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String newSystemInfo = Provider.of<ConversationProvider>(context, listen: false)
+            .currentConversation
+            .systemInfo;
+        return AlertDialog(
+          title: const Text('Edit System Info'),
+          content: TextField(
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              hintText: 'Enter new system info',
+            ),
+            controller: TextEditingController(text: newSystemInfo),
+            onChanged: (value) {
+              newSystemInfo = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: const Text(
+                'Save',
+                style: TextStyle(
+                  color: Color(0xff55bb8e),
+                ),
+              ),
+              onPressed: () {
+                Provider.of<ConversationProvider>(context, listen: false)
+                    .updateSystemInfo(newSystemInfo);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
+
+
